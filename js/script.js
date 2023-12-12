3c9ee3a06d1c239454af7620b1f9aaaa3248237b
zhow <zihao.zio14@gmail.com>
2023-12-12 14:17:36 +0800

    fixed slideshow

--- js/script.js
+++ js/script.js
@@ -11,3 +11,16 @@ document.querySelector(".overlay").addEventListener("click", () => {
   document.querySelector(".overlay").classList.toggle("show");
   
 });
+
+let i = 0;
+let curr = document.getElementById("slide"+(i+1));
+curr.classList.toggle("fade");
+
+setInterval(() => {
+  
+  curr.classList.toggle("fade");
+  i = (i+1)%3;
+  curr = document.getElementById("slide"+(i+1));
+  curr.classList.toggle("fade");
+  
+}, 3000);

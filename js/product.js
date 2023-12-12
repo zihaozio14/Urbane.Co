7f0d3ddff34d6fa45ad27591a765c2c1ac400e7d
zhow <zihao.zio14@gmail.com>
2023-12-13 00:39:36 +0800

    change script

--- js/product.js
+++ js/product.js
@@ -4,23 +4,32 @@ let select = document.getElementById("promo");
 document.getElementById("promo").addEventListener("click", () => {
 
   active.classList.toggle("toggle");
+  select.classList.toggle("toggle");
   active = document.getElementById("promotion");
+  select = document.getElementById("promo");
   active.classList.toggle("toggle");
+  select.classList.toggle("toggle");
 
 });
 
 document.getElementById("new").addEventListener("click", () => {
 
   active.classList.toggle("toggle");
+  select.classList.toggle("toggle");
   active = document.getElementById("new-in");
+  select = document.getElementById("new");
   active.classList.toggle("toggle");
+  select.classList.toggle("toggle");
 
 });
 
 document.getElementById("ret").addEventListener("click", () => {
 
   active.classList.toggle("toggle");
+  select.classList.toggle("toggle");
   active = document.getElementById("retail");
+  select = document.getElementById("ret");
   active.classList.toggle("toggle");
+  select.classList.toggle("toggle");
 
 });

44ed2e97ec62ae19356c5e8834b7c96becb0d780
zhow <zihao.zio14@gmail.com>
2023-12-12 22:23:01 +0800

    fixed event listener

--- js/product.js
+++ js/product.js
@@ -1,25 +1,25 @@
 let active = document.getElementById("promotion");
 
-document.getElementById("promotion").addEventListener("click", () => {
+document.getElementById("promo").addEventListener("click", () => {
 
   active.classList.toggle("toggle");
   active = document.getElementById("promotion");
   active.classList.toggle("toggle");
 
-})
+});
 
-document.getElementById("new-in").addEventListener("click", () => {
+document.getElementById("new").addEventListener("click", () => {
 
   active.classList.toggle("toggle");
   active = document.getElementById("new-in");
   active.classList.toggle("toggle");
 
-})
+});
 
-document.getElementById("retail").addEventListener("click", () => {
+document.getElementById("ret").addEventListener("click", () => {
 
   active.classList.toggle("toggle");
   active = document.getElementById("retail");
   active.classList.toggle("toggle");
 
-})
+});

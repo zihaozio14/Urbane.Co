6f83b62616989a1675af514fa6bdb10785926baf
zhow <zihao.zio14@gmail.com>
2023-12-12 22:15:01 +0800

    fixed script

--- js/script.js
+++ js/script.js
@@ -36,20 +36,23 @@ let active = document.getElementById("promotion");
 document.getElementById("promotion").addEventListener("click", () => {
 
   active.classList.toggle("toggle");
-  document.getElementById("promotion").classList.toggle("toggle");
+  active = document.getElementById("promotion");
+  active.classList.toggle("toggle");
 
 })
 
 document.getElementById("new-in").addEventListener("click", () => {
 
   active.classList.toggle("toggle");
-  document.getElementById("new-in").classList.toggle("toggle");
+  active = document.getElementById("new-in");
+  active.classList.toggle("toggle");
 
 })
 
 document.getElementById("retail").addEventListener("click", () => {
 
   active.classList.toggle("toggle");
-  document.getElementById("retail").classList.toggle("toggle");
+  active = document.getElementById("retail");
+  active.classList.toggle("toggle");
 
 })

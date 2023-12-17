b663c764203ece3dca3e2147744b84016aab19eb
JerrYu0301e <jerryyu@hotmail.co.uk>
2023-12-17 23:43:12 +0800

    FINAL

--- js/product.js
+++ js/product.js
@@ -1,34 +1,29 @@
 let active = document.getElementById("promotion");
+let select = document.getElementById("promo");
 
 document.getElementById("promo").addEventListener("click", () => {
-
   active.classList.toggle("toggle");
   select.classList.toggle("toggle");
   active = document.getElementById("promotion");
   select = document.getElementById("promo");
   active.classList.toggle("toggle");
   select.classList.toggle("toggle");
-
 });
 
 document.getElementById("new").addEventListener("click", () => {
-
   active.classList.toggle("toggle");
   select.classList.toggle("toggle");
   active = document.getElementById("new-in");
   select = document.getElementById("new");
   active.classList.toggle("toggle");
   select.classList.toggle("toggle");
-
 });
 
 document.getElementById("ret").addEventListener("click", () => {
-
   active.classList.toggle("toggle");
   select.classList.toggle("toggle");
   active = document.getElementById("retail");
   select = document.getElementById("ret");
   active.classList.toggle("toggle");
   select.classList.toggle("toggle");
-
 });
